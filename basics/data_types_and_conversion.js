13f2e586848b6af8ca231192ef3b1cf77423536f
Aakash323 <160621659+Aakash323@users.noreply.github.com>
2024-09-27 16:54:07 +0000

    made some changes

---
+++ basics/data_types_and_conversion.js
@@ -0,0 +1,53 @@
+let number = 123
+//this is a number
+
+
+let string = "this is a string"
+//this is a string
+
+
+let string1 ="15451"
+//this is a string
+
+
+let isLoggedIn = false
+console.log(typeof (isLoggedIn));
+// this is a boolean
+
+
+let school =Number("hello")
+console.log(school)
+console.log(typeof (school));
+// String is converted into a number 
+
+
+let students = ['ram','hari','alex'] // this is an array
+console.log(students);
+console.log(typeof(students));
+// we get object when we use typeof of the array
+
+
+const myobj ={    
+    name:'Alex',
+    age: 21,
+    address: 'Nepal'
+}
+console.log(myobj);
+console.log(typeof(myobj));
+// this is an object
+
+
+const id = Symbol(2121);
+console.log(id);
+console.log(typeof(id));
+// this is a symbol data type used for unique values
+
+
+
+
+
+
+
+
+
+
