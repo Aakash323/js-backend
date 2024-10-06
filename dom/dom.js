0acd5d429c61800ddcce49f40ec4b9787134f78e
Aakash Poudel <160621659+Aakash323@users.noreply.github.com>
2024-10-06 19:16:05 +0000

    dom

---
+++ dom/dom.js
@@ -0,0 +1,10 @@
+// DOM (Document Object Model)
+// dom selectors
+
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+// document.getElementsByTagNameNS
+// document.getElementsByName
+// document.querySelector
+// document.querySelectorAll
