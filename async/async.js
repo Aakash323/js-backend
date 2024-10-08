80be49bb4872514e85122c37865c6ec1c816dee4
Aakash Poudel <160621659+Aakash323@users.noreply.github.com>
2024-10-08 18:57:02 +0000

    async

---
+++ async/async.js
@@ -0,0 +1,4 @@
+// methods like setInterval(func , time) setTimout(func , time) are used to implemenmt async js
+// other useful methods are clearInterval(reference)  and clearTimeout()
+
+// setInterval runs until stop setTimeout runs only 1 time
