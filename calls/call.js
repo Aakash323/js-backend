842f47cc4d5d907ba1e6c0ac2e716723328b0f71
Aakash Poudel <160621659+Aakash323@users.noreply.github.com>
2024-10-15 15:31:54 +0000

    oop

--- calls/call.js
+++ calls/call.js
@@ -6,7 +6,7 @@ function game(name){
 }
 function gamess(name,id, players){
   
-    game.call(this,name)  // game.call is calling another function and this is taken as reference of game function
+    game.call(this,name)  // game.call is calling another function and [this] is taken as reference of game function
     this.id = id
     this.players= players
 }
